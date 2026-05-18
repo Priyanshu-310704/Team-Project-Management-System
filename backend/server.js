264c021123ae46440e2807368c43a4734b393f39
Priyanshu <prysh310704@gmail.com>
2026-05-19 01:27:03 +0530

    Added GitHub Actions 1

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+      console.log(`Server running✈️ on port ${process.env.PORT}`);
     });
   })
   .catch((err) => console.error(err));
